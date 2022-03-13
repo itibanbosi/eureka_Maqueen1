1336ff1d9be0e287be77a2b545b94eb52bd52d88
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2022-03-13 19:34:12 +0900

    更新 pxt.json, main.blocks, main.ts, Eureka_Maqueen1.ts

--- Eureka_Maqueen1.ts
+++ Eureka_Maqueen1.ts
@@ -12,11 +12,13 @@ namespace eureka_blocks_car {
 
 
 //% color="#3943c6" weight=70　blockId=servos_direction
-//% block="|%sinkou_houkou|へ |%time_sec| 秒間進む" group="2　基本の動き"
-export function car_derection_time(sinkou_houkou: direction, time_sec: number): void {
+//% block="|%sinkou_houkou|へ |%time_sec| 秒間進む 出力 |%Power|" group="2　基本の動き"
+//% advanced=true
+//% Power.min=0 Power.max=255
+export function car_derection_time(sinkou_houkou: direction, time_sec: number,Power:number): void {
     switch (sinkou_houkou) {
         case direction.前:
-            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100);
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW,Power );
             basic.pause(time_sec * 1000);
             maqueen.motorStop(maqueen.Motors.All);
             basic.pause(100);
@@ -25,13 +27,13 @@ export function car_derection_time(sinkou_houkou: direction, time_sec: number): 
 
         case direction.左:
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0);
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100);
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Power);
             basic.pause(time_sec * 1000);
             maqueen.motorStop(maqueen.Motors.All);
             basic.pause(100);
             break;
         case direction.右:
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100);
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, Power);
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0);
             basic.pause(time_sec * 1000);
             maqueen.motorStop(maqueen.Motors.All);
